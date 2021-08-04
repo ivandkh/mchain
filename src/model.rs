6c327080b35b78cfda251eda276cac34e04b2fcd
sustxh <sustinianvanxh@gmail.com>
2021-08-05 01:18:48 +0300

    fit_ngrams bug fixes

--- src/model.rs
+++ src/model.rs
@@ -1,18 +1,17 @@
-use std::collections::HashMap;
 use rand::seq::SliceRandom;
+use std::collections::HashMap;
 
 pub struct Model<'a> {
-    pub chain:HashMap<&'a str, Vec<&'a str>>,
+    pub chain: HashMap<&'a str, Vec<String>>,
     startwords: Option<Vec<&'a str>>,
     _stopwords: Option<Vec<&'a str>>,
-    #[allow(dead_code)]
     seq_length: i32,
     is_fitted: bool,
 }
 
 impl<'a> Model<'a> {
-    pub fn new(seq_length: i32) -> Model<'a>{
-        Model{
+    pub fn new(seq_length: i32) -> Model<'a> {
+        Model {
             chain: HashMap::new(),
             startwords: None,
             _stopwords: None,
@@ -21,14 +20,18 @@ impl<'a> Model<'a> {
         }
     }
 
-    pub fn fit_ngrams(&mut self, ngrams:Vec<Vec<&'a str>> ) {
-
-        for ngram in ngrams{
+    pub fn fit_ngrams(&mut self, ngrams: Vec<Vec<&'a str>>) {
+        for ngram in ngrams {
             let key = ngram[0];
-            let val = Vec::from(&ngram[1..]);
-            //TODO:
-            //Get around this vector recreation
-            self.chain.insert(key, val);
+            //let new_val = Vec::from(&ngram[1..]);
+            let new_val = ngram[1..].join(" ");
+
+            match self.chain.get_mut(key) {
+                Some(val) => val.push(new_val),
+                None => {
+                    self.chain.insert(key, vec![new_val]);
+                }
+            }
         }
         self.is_fitted = true
     }
@@ -43,36 +46,29 @@ impl<'a> Model<'a> {
         unimplemented!()
     }
 
-
-    pub fn generate(self, start_word: Option<&str>) -> String{
-
+    pub fn generate(self, start_word: Option<&str>) -> String {
         let mut output = String::new();
 
-        if let Some(word) = start_word{
+        if let Some(mut word) = start_word {
             output.push_str(&format!("{} ", &word));
 
-            for _ in 1..self.seq_length{
-                match self.chain.get(word){
+            for _ in 1..self.seq_length {
+                match self.chain.get(word) {
                     Some(words) => {
                         let mut rng = rand::thread_rng();
-                        let word = words.choose(&mut rng).unwrap();
+                        word = words.choose(&mut rng).unwrap();
+
                         output.push_str(&format!("{} ", &word));
-                    },
-                    None => break
+                    }
+                    None => break,
                 }
             }
-
-        }
-
-        else if let Some(_words) = self.startwords{
+        } else if let Some(_words) = self.startwords {
             unreachable!()
-        }
-
-        else{
+        } else {
             panic!("No start words specified.")
         };
 
         output + ":)"
     }
-
 }
