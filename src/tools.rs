79c0d55a63fdd0a1170d013664b7588fb075cd73
sustxh <sustinianvanxh@gmail.com>
2021-08-05 14:27:16 +0300

    get_startwords added

--- src/tools.rs
+++ src/tools.rs
@@ -9,3 +9,27 @@ pub fn get_ngrams(n: usize, text: &str) -> Vec<Vec<&str>> {
 
     ngrams
 }
+
+pub fn get_startwords(text: &str) -> Vec<String> {
+    text.split_whitespace()
+        .filter(|word| word.contains("."))
+        .map(|word| word.replace(".", ""))
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_startwords() {
+        assert_eq!(
+            get_startwords("test. test Test. test. test"),
+            [
+                String::from("test"),
+                String::from("Test"),
+                String::from("test")
+            ]
+        );
+    }
+}
