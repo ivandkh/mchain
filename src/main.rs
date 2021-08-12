53dbf1590e1d80f87731bdb7c395435ec42336ad
sustxh <sustinianvanxh@gmail.com>
2021-08-12 16:21:19 +0300

    usage example

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
 mod model;
 mod tools;
+use std::fs;
 
 fn main() {
-    let text = String::from("It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).");
 
+    //Read file to String
+    let text = fs::read_to_string("text1.txt").expect("Can't open file.");
+
+    //Specify sequence length and create a Model
+    let mut chain = model::Model::new(6);
+
+    //Fit ngrams to the Model
     let ngrams = tools::get_ngrams(4, &text);
-    let startwords = tools::get_startwords(&text);
-    //println!("{:?}", ngrams);
-
-    let mut chain = model::Model::new(20);
     chain.fit_ngrams(ngrams);
-    chain.fit_startwords(startwords);
 
-    let output = chain.generate(Some(&String::from("it")));
-    println!("{:?}", output);
+    //Optional: fit start and stopwords to the Model
+    let startwords = tools::get_startwords(&text);
+    let stopwords = tools::get_stopwords(&text);
+    chain.fit_startwords(startwords);
+    chain.fit_stopwords(stopwords);
+
+    //Generate sequences
+    let startwith: Option<&String> = None;
+    for _ in 0..100 {
+        let output = chain.generate(startwith);
+        println!("{:?}\n", output);
+    }
 }
